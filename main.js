c16696fbbf0e079d122afc118d85ce3c04c4df59
josiahwebb <jj.webb520@gmail.com>
2022-01-16 16:28:48 -0700

    finished tutorial, now to just mess around with it

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui'
+import gsap from 'gsap'
 import { VertexColors } from 'three';
 
 
@@ -8,22 +9,22 @@ import { VertexColors } from 'three';
 const gui = new dat.GUI()
 const world = {
   plane: {
-    width: 10,
-    height: 10, 
-    widthSegments: 10,
-    heightSegments: 10
+    width: 400, //change these to adjust initial scene on load
+    height: 400, 
+    widthSegments: 50,
+    heightSegments: 50
   }
 }
-gui.add(world.plane, 'width', 1, 20).onChange(() => {
+gui.add(world.plane, 'width', 1, 500).onChange(() => {
   generatePlane()
 })
-gui.add(world.plane, 'height', 1, 20).onChange(() => {
+gui.add(world.plane, 'height', 1, 500).onChange(() => {
   generatePlane()
 })
-gui.add(world.plane, 'widthSegments', 1, 50).onChange(() => {
+gui.add(world.plane, 'widthSegments', 1, 100).onChange(() => {
   generatePlane()
 })
-gui.add(world.plane, 'heightSegments', 1, 50).onChange(() => {
+gui.add(world.plane, 'heightSegments', 1, 100).onChange(() => {
   generatePlane()
 })
 
@@ -31,13 +32,36 @@ gui.add(world.plane, 'heightSegments', 1, 50).onChange(() => {
 function generatePlane() {
   planeMesh.geometry.dispose()
   planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments)
-  const {array} = planeMesh.geometry.attributes.position
-  for (let i = 0; i < array.length; i += 3) {
+ 
+  const colors = []
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(0,.19,.4)
+  }
+
+  planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3))
+  
+  // vertex position randomization
+  const {array} = planeMesh.geometry.attributes.position //decontruts object
+  const randomValues = []
+  for (let i = 0; i < array.length; i++) {
+    
+    if (i % 3 === 0){
       const x = array[i]
       const y = array[i + 1]
       const z = array[i + 2]
-      array[i + 2] = z + Math.random()
+
+      array[i] = x + (Math.random() - 0.5) * 25
+      array[i + 1] = y + (Math.random() - 0.5) * 50
+      array[i + 2] = z + (Math.random() - .5) * 5
+    }
+
+    randomValues.push(Math.random() * Math.PI * 2)
   }
+
+
+  planeMesh.geometry.attributes.position.randomValues = randomValues
+  planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.attributes.position.array
+
 }
 
 //three requirements to set 'stage'
@@ -52,9 +76,9 @@ renderer.setPixelRatio(devicePixelRatio) //sets scene pixel density to same as d
 document.body.appendChild(renderer.domElement) 
 
 new OrbitControls(camera, renderer.domElement) //creates orbit controls (allows you to move camera)
-camera.position.z = 5 //by default camera is at same position as object in scene, this 'takes a step back'
+camera.position.z = 450 //by default camera is at same position as object in scene, this 'takes a step back'
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10) //creates shape in scene (just a plane in this case)
+const planeGeometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.widthSegments, world.plane.heightSegments) //creates shape in scene (just a plane in this case)
 const planeMaterial = new THREE.MeshPhongMaterial({ //material paints the shape
     // color: 0xff0000, 
     side: THREE.DoubleSide,
@@ -64,47 +88,97 @@ const planeMaterial = new THREE.MeshPhongMaterial({ //material paints the shape
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial) //paraphrasing, but mesh combines the two and sets vertices (prolly wrong)
 scene.add(planeMesh)
 
+generatePlane()
+
 const light = new THREE.DirectionalLight(0xffffff, 1) //provides light, phong material requires light
-light.position.set(0, 0, 1) //x,y,z coordinates (right/left, up/down, forward/backward thru screen)
+light.position.set(0, -1, 1) //x,y,z coordinates (right/left, up/down, forward/backward thru screen)
 scene.add(light) //scene.add adds the argument to scene
 
 const backLight = new THREE.DirectionalLight(0xffffff, 1) //provides light, phong material requires light
 backLight.position.set(0, 0, -1) //x,y,z coordinates (right/left, up/down, forward/backward thru screen)
 scene.add(backLight) //scene.add adds the argument to scene
 
-console.log(planeMesh.geometry.attributes)
-
-const {array} = planeMesh.geometry.attributes.position //decontruts object
-for (let i = 0; i < array.length; i += 3) {
-    const x = array[i]
-    const y = array[i + 1]
-    const z = array[i + 2]
-    array[i + 2] = z + Math.random()
-}
-
-const colors = []
-for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-  colors.push(0,0,1)
-}
-console.log(colors)
-
-planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3))
 
 const mouse = {
   x: undefined,
   y: undefined
 }
 
+let frame = 0
 function animate () {
     requestAnimationFrame(animate) //when animate() called, creates loop so scene is constantly visible
     renderer.render(scene, camera)
-    
     // planeMesh.rotation.y += .01
-    
     raycaster.setFromCamera(mouse, camera)
+    frame += 0.01
+
+    const {array, originalPosition, randomValues} = planeMesh.geometry.attributes.position
+    for(let i = 0; i < array.length; i += 3) {
+      //x
+      array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * .01
+      //y
+      array[i + 1] = originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * .01
+      //z
+      array[i + 2] = originalPosition[i + 2] + Math.cos(frame + randomValues[i + 2]) * .01
+      
+    }
+
+    planeMesh.geometry.attributes.position.needsUpdate = true
+
     const intersects = raycaster.intersectObject(planeMesh)
     if(intersects.length > 0) {
-      
+      const {color} = intersects[0].object.geometry.attributes
+
+      // vertex 1
+      color.setX(intersects[0].face.a, .1)
+      color.setY(intersects[0].face.a, .5)
+      color.setZ(intersects[0].face.a, 1)
+
+      // vertex 2
+      color.setX(intersects[0].face.b, .1)
+      color.setY(intersects[0].face.b, .5)
+      color.setZ(intersects[0].face.b, 1)
+
+      //vertex 3
+      color.setX(intersects[0].face.c, .1)
+      color.setY(intersects[0].face.c, .5)
+      color.setZ(intersects[0].face.c, 1)
+
+      intersects[0].object.geometry.attributes.color.needsUpdate = true
+
+      const initialColor = {
+        r: 0,
+        g: .19,
+        b: .4
+      }
+
+      const hoverColor = {
+        r: .1,
+        g: .5,
+        b: 1
+      }
+      gsap.to(hoverColor, {
+        r: initialColor.r,
+        g: initialColor.g,
+        b: initialColor.b,
+        duration: 1,
+        onUpdate: () => {
+          // vertex 1
+          color.setX(intersects[0].face.a, hoverColor.r)
+          color.setY(intersects[0].face.a, hoverColor.g)
+          color.setZ(intersects[0].face.a, hoverColor.b)
+
+          // vertex 2
+          color.setX(intersects[0].face.b, hoverColor.r)
+          color.setY(intersects[0].face.b, hoverColor.g)
+          color.setZ(intersects[0].face.b, hoverColor.b)
+
+          //vertex 3
+          color.setX(intersects[0].face.c, hoverColor.r)
+          color.setY(intersects[0].face.c, hoverColor.g)
+          color.setZ(intersects[0].face.c, hoverColor.b)
+        }
+      })
     }
 }
 
@@ -117,5 +191,4 @@ addEventListener('mousemove', (event) => {
 
 })
 
-
-//STOPPED AT 1:30ish
+//STOPPED AT 2:12ish
